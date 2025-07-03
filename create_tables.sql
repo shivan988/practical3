4aec948f3306a78d5437a97b25e0e966cda5a6f6
Shivpoojan Jaiswar <shivpoojanjaiswar9819@gmail.com>
2025-07-03 19:38:13 +0530

    first commit

---
+++ create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students ( 
+id INT PRIMARY KEY AUTO_INCREMENT, 
+name VARCHAR(100), 
+marks INT 
+); 
+INSERT INTO students (name, marks) VALUES 
+('Om', 85), 
+('Suresh', 90), 
+('Ramesh', 76);
