eb31ad2048bd5a50d0cf594bf7cb2d57a8edbac8
Shivpoojan Jaiswar <shivpoojanjaiswar9819@gmail.com>
2025-07-03 19:42:43 +0530

    second commit

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,12 @@
-CREATE TABLE students ( 
-id INT PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(100), 
-marks INT 
-); 
-INSERT INTO students (name, marks) VALUES 
-('Om', 85), 
-('Suresh', 90), 
-('Ramesh', 76);
+USE demo;
+
+CREATE TABLE students (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
+  marks INT
+);
+
+INSERT INTO students (name, marks) VALUES
+  ('Om', 85),
+  ('Suresh', 90),
+  ('Ramesh', 76);
